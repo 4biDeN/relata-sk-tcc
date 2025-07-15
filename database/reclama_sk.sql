85f990b9e02de5b738633302ff6648a4f778091e
4biDeN <cristianmila02@gmail.com>
2025-07-15 16:30:23 -0300

    Instalado pacote npm node, express e criado tabela de usuários da database

---
+++ database/reclama_sk.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY,
+    user_username TEXT NOT NULL,
+    user_email TEXT UNIQUE NOT NULL,
+    user_documento VARCHAR(11) NOT NULL,
+    user_password TEXT NOT NULL,
+    user_salt TEXT NOT NULL,
+    user_tipo VARCHAR(20) NOT NULL CHECK (user_tipo IN ('cidadao', 'gestor'))
+);
+
