59eb251a07418752d90cad2f1418b007c14e7bf5
4biDeN <cristianmila02@gmail.com>
2025-08-13 22:23:28 -0300

    Incluído rota de login no swagger, adicionado cookie-parser e jwt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const cookieParser = require('cookie-parser');
 const port = 3000;
 
 app.use(express.json());
@@ -10,5 +11,11 @@ require('./routes')(app);
 
 app.use('/v1/docs', express.static('./src/views'));
 app.use('/docs/swagger.yaml', express.static('./src/docs/swagger.yaml'));
+app.use(cookieParser());
 
 app.listen(port);
+
+app.get('/', (req, res) => {
+    res.cookie('myCookie', 'Testing Cookie');
+    res.send('API is running');
+});
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,11 @@
 const loginController = require('../controllers/login')
 
 module.exports = (app) => {
-    app.post('/login', loginController.login)
+    app.post('/login', loginController.login
+    /*
+        #swagger.tags = ["Login"]
+        #swagger.summary = "Realiza o login do usuário"
+        #swagger.description = 'Realiza o login do usuário com as credenciais fornecidas.'
+        
+    */);
 }
