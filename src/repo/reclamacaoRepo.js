20afa4683da03cd77de6624c20fc029d261b736f
4biDeN <cristianmila02@gmail.com>
2025-09-01 21:55:49 -0300

    feat/correcao-sql Corrgido repositório de SQLs para utilizar uma padronização no CRUD

--- src/repo/reclamacaoRepo.js
+++ src/repo/reclamacaoRepo.js
@@ -2,14 +2,14 @@ const db = require('../configs/pg')
 
 const createReclamacao = async ({ reclamacao_titulo, reclmamacao_descricao, reclamacao_status, reclamacao_user_id }) => {
     const sql = `
-    INSERT INTO t_reclamacao (
+    insert into t_reclamacao (
         reclamacao_titulo,
         reclamacao_descricao,
         reclamacao_status,
         reclamacao_user_id
     )
-    VALUES ($1, $2, $3, $4)
-    RETURNING reclamacao_id, reclamacao_titulo, reclamacao_descricao, reclamacao_status, reclamacao_user_id
+    values ($1, $2, $3, $4)
+    returning reclamacao_id, reclamacao_titulo, reclamacao_descricao, reclamacao_status, reclamacao_user_id
     `;
 
     const result = await db.query(sql, [
@@ -23,17 +23,17 @@ const createReclamacao = async ({ reclamacao_titulo, reclmamacao_descricao, recl
 
 const getReclamacaoById = async (reclamacao_id) => {
     const sql = `
-    SELECT 
+    select 
         r.reclamacao_id,
         r.reclamacao_titulo,
         r.reclamacao_descricao,
         r.reclamacao_data,
         rs.reclamacao_status_nome,
         u.user_username
-    FROM t_reclamacao r
-    JOIN t_reclamacao_status rs ON r.reclamacao_status = rs.reclamacao_status_id
-    JOIN t_usuario u ON r.reclamacao_user_id = u.user_id
-    WHERE r.reclamacao_id = $1 
+    from t_reclamacao r
+    join t_reclamacao_status rs on r.reclamacao_status = rs.reclamacao_status_id
+    join t_usuario u ON r.reclamacao_user_id = u.user_id
+    where r.reclamacao_id = $1 
     `;
 
     const result = await db.query(sql, [reclamacao_id]);
--- src/repo/userRepo.js
+++ src/repo/userRepo.js
@@ -2,10 +2,10 @@ const db = require('../configs/pg');
 
 const findByDocumneto = async (documento) => {
     const sql = `
-        SELECT u.user_id, u.user_documento, u.user_password, u.user_salt, u.user_token_version, t.user_type_name
-        FROM t_usuario u
-        INNER JOIN t_user_type t ON u.user_tipo = t.user_type_id
-        WHERE u.user_doumento = $1
+        select u.user_id, u.user_documento, u.user_password, u.user_salt, u.user_token_version, t.user_type_name
+        from t_usuario u
+        inner join t_user_type t on u.user_tipo = t.user_type_id
+        where u.user_doumento = $1
 `;
     const result = await db.query(sql, [documento]);
     return result.rows.length ? result.rows[0] : null;
@@ -13,7 +13,7 @@ const findByDocumneto = async (documento) => {
 
 const createUser = async ({ user_username, user_email, user_documento, user_password, user_salt }) => {
     const sql = `
-    INSERT INTO t_usuario (
+    insert into t_usuario (
         user_username, 
         user_email, 
         user_documento, 
@@ -21,8 +21,8 @@ const createUser = async ({ user_username, user_email, user_documento, user_pass
         user_salt, 
         user_token_version
         )
-    VALUES ($1, $2, $3, $4, $5, 0)
-    RETURNING user_id, user_username, user_email, user_documento
+    values ($1, $2, $3, $4, $5, 0)
+    returning user_id, user_username, user_email, user_documento
     `;
 
     const result = await db.query(sql, [
@@ -37,8 +37,8 @@ const createUser = async ({ user_username, user_email, user_documento, user_pass
 
 const getAll = async () => {
     const sql = `
-        SELECT user_id, user_username, user_email, user_documento, user_tipo, user_token_version
-        FROM t_usuario
+        select u.user_id, u.user_username, u.user_email, u.user_documento, u.user_tipo, u.user_token_version
+        from t_usuario u
     `;
     const result = await db.query(sql);
     return result.rows;
@@ -46,9 +46,9 @@ const getAll = async () => {
 
 const getById = async (user_id) => {
     const sql = `
-        SELECT user_id, user_username, user_email, user_documento, user_tipo, user_token_version
-        FROM t_usuario
-        WHERE user_id = $1
+        select u.user_id, u.user_username, u.user_email, u.user_documento, u.user_tipo, u.user_token_version
+        from t_usuario u
+        where u.user_id = $1
     `;
     const result = await db.query(sql, [user_id]);
     return result.rows.length ? result.rows[0] : null;
@@ -73,9 +73,9 @@ const updateUser = async (user_id, fields) => {
     console.log(values)
     values.push(user_id);
     const sql = `
-        UPDATE t_usuario
-        SET ${setClauses.join(', ')}
-        WHERE user_id = $${index}
+        update t_usuario
+        set ${setClauses.join(', ')}
+        where user_id = $${index}
     `;
     console.log(sql)
     const result = await db.query(sql, values);
@@ -84,9 +84,9 @@ const updateUser = async (user_id, fields) => {
 
 const incrementTokenVersion = async (userId) => {
     const sql = `
-        UPDATE t_usuario
-        SET user_token_version = user_token_version + 1
-        WHERE user_id = $1
+        update t_usuario
+        set user_token_version = user_token_version + 1
+        where user_id = $1
     `;
     await db.query(sql, [userId]);
 };
